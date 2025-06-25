341bb46f3a50709aefc6a40ea3305e8f8307f405
hackerjack9 <lucekl03@frontier.com>
2025-06-25 08:02:05 -0400

    onsearch

--- movie.js
+++ movie.js
@@ -4,16 +4,20 @@ const searchInput = document.getElementById('search');
 const movieContainer = document.getElementById('movie-container');
 
 // Add event listener to the search button
-searchButton.addEventListener('click', async (fast) => {
+searchButton.addEventListener('click', async () => {
     const searchTerm = searchInput.value; // Get the search term from input
     const apiKey = 'https://www.omdbapi.com/?apikey=b5b98c80'; // Replace with your actual API key
     const response = await fetch(`http://www.omdbapi.com/?s=fast&apikey=b5b98c80`);
     const data = await response.json(); // Parse the JSON response
     
+     function onSearchChange(event) {
+        console.log(event.target.value)
+    }
+
     // Clear previous results
     movieContainer.innerHTML = '';
 
-
+    
     // Check if the response is successful
     if (data.Response === 'True') {
         // Loop through the results and create movie items
@@ -33,7 +37,3 @@ searchButton.addEventListener('click', async (fast) => {
 });
 
 
-//The API is called every single time a user stops typing on the search input, and we can do that using the onChange event in the input.
-//  Then inside of it we need to find a way to have access to whatever the user is typing, pass it to the function, and use it in the API 
-// so it returns to us a different content based on that value, so if I search for batman, it will show to me the first ten movies related 
-// to the word batman.
